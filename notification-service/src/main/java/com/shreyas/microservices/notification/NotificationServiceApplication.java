bcf3158820b62f391c1501f60ed12460caf0e31f
Shreyas Bhosale <bhosaleshreyas53@gmail.com>
2024-09-26 13:07:39 +0530

    notification-service-commit

---
+++ notification-service/src/main/java/com/shreyas/microservices/notification/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shreyas.microservices.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
